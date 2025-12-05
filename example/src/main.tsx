e8896d5ef5afd2d4d6e93272abce94bda87bba50
xu.pengfei <1508075252@qq.com>
2025-12-05 16:40:07 +0800

    1

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
-import '@fabernpm/lib01/dist/lib01.css';
+import '@fabernpm/lib01/style.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
